a1cb7a587868976a8b67c232d3dde6eaa8199f68
Prashant Vishwakarma <146864416+slayerboy228@users.noreply.github.com>
2025-08-15 22:25:19 +0530

    Create 34B - Sale.java

---
+++ 34B - Sale.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class problem34B {
+
+	public static void main(String[] args) {
+		// input section
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int sets[] = new int[n];
+		for (int i = 0; i < n; i++)
+			sets[i] = sc.nextInt();
+
+		// output section
+    Arrays.sort(sets);
+		int earn = 0;
+		for (int i = 0; i < n; i++) {
+			if (sets[i] < 0) {
+				earn += Math.abs(sets[i]);
+				m--;
+				if (m == 0)
+					break;
+			}
+		}
+		System.out.println(earn);
+		sc.close();
+	}
+}
